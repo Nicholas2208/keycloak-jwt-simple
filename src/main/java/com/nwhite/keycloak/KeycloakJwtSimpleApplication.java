b1854d63626922e66246b1f7ac70535b452fd238
Nicholas2208 <nikolaj.belyakov.84@mail.ru>
2026-07-26 21:36:08 +0300

    Initial commit

---
+++ src/main/java/com/nwhite/keycloak/KeycloakJwtSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.nwhite.keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakJwtSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakJwtSimpleApplication.class, args);
+	}
+
+}
